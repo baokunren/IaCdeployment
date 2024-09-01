60f178858926a67569b9fb60a276147be5bfe83a
baokunren <60170881+baokunren@users.noreply.github.com>
2024-09-02 11:07:31 +1200

    Create a Deployment Share

--- InstallMDT.ps1
+++ InstallMDT.ps1
@@ -6,3 +6,20 @@ Install-ADDSForest -DomainName "IaCtestServer.com" -InstallDNS -SafeModeAdminist
 
 # Install Windows MDT
 Start-Process -FilePath "C:\deploymentsoftware\MicrosoftDeploymentToolkit_x64.msi" -ArgumentList "/quiet", "/norestart" -Wait
+
+
+
+# Script to automate the deployment of Windows 11 using MDT and WDS
+
+# Step 1: Import the MDT module
+Import-Module "C:\Program Files\Microsoft Deployment Toolkit\Bin\MicrosoftDeploymentToolkit.psd1"
+Write-Output "MDT module imported successfully."
+
+# Step 2: Create a Deployment Share
+$deploymentSharePath = "C:\DeploymentShare"
+$PSDriveName = "DS001"
+$Description = "MDT Deployment Share"
+$ShareName="DeploymentShare$"
+New-PSDrive -Name $PSDriveName -PSProvider "MDTProvider" -Root $deploymentsharePath -Description $Description -NetworkPath \\$env:COMPUTERNAME\$ShareName | add-MDTPersistentDrive
+Write-Output "Deployment share created at $deploymentSharePath."
+
