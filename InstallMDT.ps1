874b72e03a2282ca029323e305d6964c7087ac6c
baokunren <60170881+baokunren@users.noreply.github.com>
2024-09-01 13:48:32 +1200

    deloying Win11 OS and apps MDT installation

---
+++ InstallMDT.ps1
@@ -0,0 +1,8 @@
+﻿# Install ADDS, DHCP, and DNS
+Install-WindowsFeature -Name AD-Domain-Services, DHCP, DNS -IncludeManagementTools
+
+# Promote to Domain Controller
+Install-ADDSForest -DomainName "IaCtestServer.com" -InstallDNS -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText "Aspire2" -Force) -Force
+
+# Install Windows MDT
+Start-Process -FilePath "C:\deploymentsoftware\MicrosoftDeploymentToolkit_x64.msi" -ArgumentList "/quiet", "/norestart" -Wait
