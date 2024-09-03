bf59e256adf6b5c6536777408a1ff6f6d7b611fe
baokunren <60170881+baokunren@users.noreply.github.com>
2024-09-04 09:16:42 +1200

    add the apps

--- InstallMDT.ps1
+++ InstallMDT.ps1
@@ -28,3 +28,21 @@ New-PSDrive -Name "DS001" -PSProvider MDTProvider -Root "C:\DeploymentShare"
 import-mdtoperatingsystem -path "DS001:\Operating Systems" -SourcePath "D:\" -DestinationFolder "Windows 11" -Verbose
 
 
+# Step 4: Import Applications into MDT
+# Import Google Chrome
+New-PSDrive -Name "DS001" -PSProvider MDTProvider -Root "C:\DeploymentShare"
+import-MDTApplication -path "DS001:\Applications" -enable "True" -Name "Google Chrome" -ShortName "Google Chrome" -Version "" -Publisher "" -Language "" -CommandLine "ChromeSetup.exe /s" -WorkingDirectory ".\Applications\Google Chrome" -ApplicationSourcePath "C:\deploymentsoftware" -DestinationFolder "Google Chrome" -Verbose
+
+
+# Import VLC Media Player
+New-PSDrive -Name "DS001" -PSProvider MDTProvider -Root "C:\DeploymentShare"
+import-MDTApplication -path "DS001:\Applications" -enable "True" -Name "VLC" -ShortName "VLC" -Version "" -Publisher "" -Language "" -CommandLine "vlc-3.0.21-win64.exe" -WorkingDirectory ".\Applications\VLC" -ApplicationSourcePath "C:\deploymentsoftware" -DestinationFolder "VLC" -Verbose
+
+
+# Import Adobe Acrobat Reader
+New-PSDrive -Name "DS001" -PSProvider MDTProvider -Root "C:\DeploymentShare"
+import-MDTApplication -path "DS001:\Applications" -enable "True" -Name "AcroReader" -ShortName "AcroReader" -Version "" -Publisher "" -Language "" -CommandLine "AcroRdrDC2400221005_en_US.exe" -WorkingDirectory ".\Applications\AcroReader" -ApplicationSourcePath "C:\deploymentsoftware" -DestinationFolder "AcroReader" -Verbose
+
+# import MDT tasks sequences
+New-PSDrive -Name "DS001" -PSProvider MDTProvider -Root "C:\DeploymentShare"
+import-mdttasksequence -path "DS001:\Task Sequences" -Name "OS with APPs" -Template "Client.xml" -Comments "" -ID "1" -Version "1.0" -OperatingSystemPath "DS001:\Operating Systems\Windows 11 Home in Windows 11 install.wim" -FullName "Windows User" -OrgName "IaCtestServer" -HomePage "about:blank" -Verbose
